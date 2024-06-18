3b0abe97f5c5dfe97b0a2350ca38bebaca94c314
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2024-06-18 16:44:56 -0300

    Trabalhando com metódos enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -14,31 +15,35 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-@Entity //anotação JPA
+@Entity // anotação JPA
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id //anotação JPA
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //anotação JPA
+
+	@Id // anotação JPA
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // anotação JPA
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	@ManyToOne //indica que o relacionamento entre as entidades é de muitos para um, ou seja, muitas instâncias da entidade que 
-	//contém essa anotação estão relacionadas a uma instância de outra entidade.
-	@JoinColumn(name = "client_id") //especifica que a coluna client_id na tabela da entidade que contém este campo será usada como
-	//a chave estrangeira para armazenar o identificador do cliente.
+	private Integer orderStatus;
+
+	@ManyToOne // indica que o relacionamento entre as entidades é de muitos para um, ou seja,
+				// muitas instâncias da entidade que
+	// contém essa anotação estão relacionadas a uma instância de outra entidade.
+	@JoinColumn(name = "client_id") // especifica que a coluna client_id na tabela da entidade que contém este campo
+									// será usada como
+	// a chave estrangeira para armazenar o identificador do cliente.
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
-	public Order( Instant moment, User client) {
+	public Order(Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +61,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
@@ -82,8 +97,5 @@ public class Order implements Serializable{
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
+
 }
